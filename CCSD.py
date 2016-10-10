de1ee357d556b076e5f534bd66740d1ee8d7629c
hakii27 <haakoek@student.matnat.uio.no>
2016-10-10 20:34:31 +0200

    Add files via upload

--- CCSD.py
+++ CCSD.py
@@ -36,7 +36,145 @@ def computeFockMatrix(N,L,w):
 			F[p,q] = delta(p+1,q+1)*energy(q+1)
 			for i in range(0,L/2):
 				F[p,q] += QRPS(p+1,i+1,q+1,i+1,w)			
-	return F	
+	return F
+
+def initialize(N,L,w):
+	F_init = computeFockMatrix(N,L,w)
+
+	t1_old = zeros((N, (L/2-N)))
+
+	for i in range(0,N):
+		for a in range(N,L/2):
+			Dia  = F_init[i,i] - F_init[a,a]
+			t_ia = F_init[a,i]/Dia
+			t1_old[i,a-N] = t_ia
+
+	k = 0
+	t2_old = zeros((N,N,(L/2-N),(L/2-N)))		
+	for i in range(0,N):
+		for j in range(0,N):
+			for a in range(N,L/2):
+				for b in range(N,L/2):
+					D_ij_ab = F_init[i,i] + F_init[j,j] - F_init[a,a] - F_init[b,b]
+					t2_old[i,j,a-N,b-N] = QRPS(a+1,b+1,i+1,j+1,w)/D_ij_ab
+	return F_init, t1_old, t2_old	
+
+#Extremely naive implementation of coupled-cluster amplitude equations.
+
+def ECCSD(E0,t1,t2,N,L,w,F):
+	E_CCSD = E0
+	
+	for i in range(0,N):
+		for a in range(N,L/2):
+			E_CCSD += F[a,i]*t1[i,a-N]
+
+	for a in range(N,L/2):
+		for b in range(N,L/2):
+			for i in range(0,N):
+				for j in range(0,N):
+					E_CCSD += 0.25*I(i,j,a,b,w)*t2[i,j,a-N,b-N]
+					E_CCSD += 0.5*I(i,j,a,b,w)*t1[i,a-N]*t1[j,b-N]
+
+	return E_CCSD
+
+def I(i,j,a,b,w):
+	return QRPS(a+1,b+1,i+1,j+1,w)
+
+def computeT1Amplitudes(N,L,w,F,t1_old,t2_old):
+	
+	t1_new = zeros((N, (L/2-N)))
+	
+	for i in range(0,N):
+		for a in range(N,L/2):
+			
+			t1_new[i,a-N] = F[a,i] # *D_ia
+			
+			for c in range(N,L/2):
+				if(c != a):
+					t1_new[i,a-N] += F[a,c]*t1_old[i,c-N]
+			
+			for k in range(0,N):
+				if(i != k):
+					t1_new[i,a-N] -= F[k,i]*t1_old[k,a-N]
+			
+			for k in range(0,N):
+				for c in range(N,L/2):
+					t1_new[i,a-N] += QRPS(c+1,i+1,k+1,a+1,w)*t1_old[k,c-N]
+					t1_new[i,a-N] += F[k,c]*t2_old[i,k,a-N,c-N]
+					t1_new[i,a-N] -= F[k,c]*t1_old[i,c-N]*t1_old[k,a-N]
+
+			for k in range(0,N):
+				for c in range(N,L/2):
+					for d in range(N,L/2):
+						t1_new[i,a-N] += 0.5*QRPS(c+1,d+1,k+1,a+1,w)*t2_old[k,i,c-N,d-N]
+						t1_new[i,a-N] += QRPS(c+1,d+1,k+1,a+1,w)*t1_old[k,c-N]*t1_old[i,d-N]
+
+			for k in range(0,N):
+				for l in range(0,N):
+					for c in range(N,L/2):
+						t1_new[i,a-N] -= 0.5*QRPS(c+1,i+1,k+1,l+1,w)*t2_old[k,l,c-N,a-N]
+						t1_new[i,a-N] -= QRPS(c+1,i+1,k+1,l+1,w)*t1_old[k,c-N]*t1_old[l,a-N]
+			
+			for k in range(0,N):
+				for l in range(0,N):
+					for c in range(N,L/2):
+						for d in range(N,L/2):							
+							t1_new[i,a-N] -= QRPS(c+1,d+1,k+1,l+1,w)*t1_old[k,c-N]*t1_old[i,d-N]*t1_old[l,a-N] 
+							t1_new[i,a-N] += QRPS(c+1,d+1,k+1,l+1,w)*t1_old[k,c-N]*t2_old[l,i,d-N,a-N]
+							t1_new[i,a-N] -= 0.5*QRPS(c+1,d+1,k+1,l+1,w)*t2_old[k,i,c-N,d-N]*t1_old[l,a-N]
+							t1_new[i,a-N] -= 0.5*QRPS(c+1,d+1,k+1,l+1,w)*t2_old[k,l,c-N,a-N]*t1_old[i,d-N]
+			
+			D_ia = F[i,i]-F[a,a]			
+			t1_new[i,a-N] = t1_new[i,a-N]/D_ia
+
+	return t1_new
+
+def computeT2Amplitudes(N,L,w,F,t1_old,t2_old):
+	t2_new = zeros((N,N,(L/2-N),(L/2-N)))
+	
+	for i in range(0,N):
+		for j in range(0,N):
+			for a in range(N,L/2):
+				for b in range(N,L/2):
+					
+					D_ij_ab = F[i,i] + F[j,j] - F[a,a] - F[b,b]					
+					t2_new[i,j,a-N,b-N] = I(a,b,i,j,w)
+
+					for k in range(0,N):
+						for l in range(0,N):
+							t2_new[i,j,a-N,b-N] += 0.5*I(k,l,i,j,w)*t2_old[k,l,a-N,b-N]
+							t2_new[i,j,a-N,b-N] += 0.5*(I(k,l,i,j,w)*t1_old[k,a-N]*t1_old[l,b-N] - I(k,l,i,j,w)*t1_old[k,b-N]*t1_old[l,a-N])
+
+					for c in range(N,L/2):
+						for d in range(N,L/2):
+							t2_new[i,j,a-N,b-N] += 0.5*I(a,b,c,d,w)*t2_old[i,j,c-N,d-N]
+
+					for k in range(0,N):
+						for c in range(N,L/2):
+							t2_new[i,j,a-N,b-N] += F[k,c]*t1_old[k,a-N]*t2_old[i,j,b-N,c-N] - F[k,c]*t1_old[k,b-N]*t2_old[i,j,a-N,c-N]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L/2):
+								for d in range(N,L/2):
+									t2_new[i,j,a-N,b-N] += 0.25*I(k,l,c,d,w)*t2_old[i,j,c-N,d-N]*t2_old[k,l,a-N,b-N]
+
+					for k in range(0,N):
+						if(k != j):						
+							t2_new[i,j,a-N,b-N] -= F[k,j]*t2_old[i,k,a-N,b-N]
+						if(k != i):
+							t2_new[i,j,a-N,b-N] += F[k,i]*t2_old[j,k,a-N,b-N]
+
+						t2_new[i,j,a-N,b-N] -= I(k,b,i,j,w)*t1_old[k,a-N] - I(k,a,i,j,w)*t1_old[k,b-N]
+
+					for c in range(N,L/2):
+						if(c != b):
+							t2_new[i,j,a-N,b-N] += F[b,c]*t2_old[i,j,a-N,c-N]
+						if(c != a):
+							t2_new[i,j,a-N,b-N] -= F[a,c]*t2_old[i,j,b-N,c-N] 
+					
+					t2_new[i,j,a-N,b-N] = t2_new[i,j,a-N,b-N]/D_ij_ab  
+	return t2_new
 
 inFile = open('coulomb.dat','r')
 w = {}
@@ -49,32 +187,17 @@ for line in inFile:
 
 L = 12
 N = 2
-def initialize(N,L,w):
-	F_init = computeFockMatrix(N,L,w)
 
-	t1_old = zeros(N*(L/2-N))
-	k = 0
-
-	for i in range(0,N):
-		for a in range(N,L/2):
-			Dia  = F_init[i,i] - F_init[a,a]
-			t_ia = F_init[a,i]/Dia
-			t1_old[k] = t_ia
-			k += 1
-
-	k = 0
-	t2_old = zeros(N**2 * (L/2-N)**2)		
-	for i in range(0,N):
-		for j in range(0,N):
-			for a in range(N,L/2):
-				for b in range(N,L/2):
-					D_ij_ab = F_init[i,i] + F_init[j,j] - F_init[a,a] - F_init[b,b]
-					t2_old[k] = QRPS(a+1,b+1,i+1,j+1,w)/D_ij_ab
-					k += 1
-	return F_init, t1_old, t2_old
 				
 F, t1_old, t2_old = initialize(N,L,w)
-print t2_old
+t1_new = computeT1Amplitudes(N,L,w,F,t1_old,t2_old)
+t2_new = computeT2Amplitudes(N,L,w,F,t1_old,t2_old)
+ 
+Enew = ECCSD(0,t1_new,t2_new,N,L,w,F)
+print Enew
+
+
+
 
 
 
