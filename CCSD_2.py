86aa0ad8b5e98d3f3cc05a16786df650f8db1c9b
hakii27 <haakoek@student.matnat.uio.no>
2016-10-23 19:20:13 +0200

    Typed in t2Amplitudes, mircale if this works

--- CCSD_2.py
+++ CCSD_2.py
@@ -60,8 +60,8 @@ def spin(x):
 
 def QRPS(q,r,p,s,w):
 	
-	key1 = str(q)+str(r) + str(p) + str(s)
-	key2 = str(q)+str(r) + str(s) + str(p)
+	key1 = str(map_index(q))+str(map_index(r)) + str(map_index(p)) + str(map_index(s))
+	key2 = str(map_index(q))+str(map_index(r)) + str(map_index(s)) + str(map_index(p))
 	
 	if((w.has_key(key1) == True) and (w.has_key(key2) == True)):
 		qrps = w[key1]-0.5*w[key2]
@@ -112,8 +112,9 @@ def computeFockMatrix2(N,L,w):
 		F[p,p] = SingleParticleEnergy(map_index(p))
 		for q in range(0,L):
 			for i in range(0,N):
-				F[p,q] += QRPS3(p,i,q,i,w)*delta(spin(p),spin(q))*delta(spin(i),spin(i))#QRPS2(p,i,q,i,w)
-				F[p,q] -= QRPS3(p,i,i,q,w)*delta(spin(p),spin(i))*delta(spin(i),spin(q))	
+				F[p,q] += QRPS2(p,i,q,i,w)
+				#F[p,q] += QRPS3(p,i,q,i,w)*delta(spin(p),spin(q))*delta(spin(i),spin(i))#QRPS2(p,i,q,i,w)
+				#F[p,q] -= QRPS3(p,i,i,q,w)*delta(spin(p),spin(i))*delta(spin(i),spin(q))	
 	return F
 
 def initialize(N,L,w,F):
@@ -134,10 +135,7 @@ def initialize(N,L,w,F):
 			for i in range(0,N):
 				for j in range(0,N):
 					Dabij = F[i,i]+F[j,j] - F[a,a] - F[b,b]
-					tdoubles[a-N,b-N,i,j] = QRPS2(a,b,i,j,w)/Dabij#QRPS3(a,b,i,j,w)*delta(spin(a),spin(i))*delta(spin(b),spin(j))
-					#tdoubles[a-N,b-N,i,j] -= QRPS3(a,b,j,i,w)*delta(spin(a),spin(j))*delta(spin(b),spin(i))
-					#tdoubles[a-N,b-N,i,j] /= Dabij
-
+					tdoubles[a-N,b-N,i,j] = QRPS2(a,b,i,j,w)/Dabij
 	return tsingles, tdoubles
 
 def ECCSD(E0,t1,t2,N,L,w,F):
@@ -161,6 +159,127 @@ def ECCSD(E0,t1,t2,N,L,w,F):
 					ECCSD += tmp1 + tmp2
 
 	return ECCSD
+
+def computeT2Amplitudes(N,L,w,t1,t2,F):
+	
+	tdoubles = np.zeros( (L-N,L-N,N,N) )
+
+	for i in range(0,N):
+		for j in range(0,N):
+			for a in range(N,L):
+				for b in range(N,L):
+					
+					tdoubles[a-N,b-N,i,j] = QRPS2(i,j,a,b,w)
+
+					for k in range(0,N):
+						for l in range(0,N):
+							tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(i,j,k,l,w)*t2[a-N,b-N,k,l]
+
+					for c in range(N,L):
+						for d in range(N,L):
+							tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(c,d,a,b,w)*t2[c-N,d-N,i,j]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								for d in range(N,L):
+									tdoubles[a-N,b-N,i,j] += 0.25*QRPS2(c,d,k,l,w)*t2[a-N,b-N,k,l]
+
+					for k in range(0,N):
+						if(k != j):
+							tdoubles[a-N,b-N,i,j] -= F[k,j]*t2[a-N,b-N,i,k]
+						if(k != i):
+							tdoubles[a-N,b-N,i,j] += F[k,i]*t2[a-N,b-N,j,k]
+
+					for c in range(N,L):
+						if(c != b):
+							tdoubles[a-N,b-N,i,j] += F[b,c]*t2[a-N,c-N,i,j]
+						if(c != a):
+							tdoubles[a-N,b-N,i,j] -= F[a,c]*t2[b-N,c-N,i,j]
+
+					#P(ab) -terms
+					for k in range(0,N):
+						for c in range(N,L):
+							tdoubles[a-N,b-N,i,j] += F[k,c]*t1[a-N,k]*t2[b-N,c-N,i,j] - F[k,c]*t1[b-N,k]*t2[a-N,c-N,i,j]
+
+					for k in range(0,N):
+						tdoubles[a-N,b-N,i,j] -= QRPS2(i,j,k,b,w)*t1[a-N,k] - QRPS2(i,j,k,a,w)*t1[b-N,k]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							tdoubles[a-N,b-N,i,j] += 0.5*(QRPS2(i,j,k,l,w)*(t1[a-N,k]*t1[b-N,l] - t1[b-N,k]*t1[a-N,l]))
+
+					for k in range(0,N):
+						for c in range(N,L):
+							for d in range(N,L):
+								  tdoubles[a-N,b-N,i,j] += QRPS2(c,d,k,a,w)*t1[c-N,k]*t2[d-N,b-N,i,j] - 0.5*QRPS2(c,d,k,b,w)*t1[a-N,k]*t2[c-N,d-N,i,j]
+								  tdoubles[a-N,b-N,i,j] -= QRPS2(c,d,k,b,w)*t1[c-N,k]*t2[d-N,a-N,i,j] - 0.5*QRPS2(c,d,k,a,w)*t1[b-N,k]*t2[c-N,d-N,i,j]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								for d in range(N,L):
+									tdoubles[a-N,b-N,i,j] += QRPS2(c-N,d-N,k,l)*(0.25*t1[a-N,k]*t1[b-N,l]*t2[c-N,d-N,i,j] - t1[c-N,k]*t1[a-N,l]*t2[d-N,b-N,i,j] - 0.5*t2[a-N,c-N,i,j]*t2[b-N,d-N,k,l])
+									tdoubles[a-N,b-N,i,j] -= QRPS2(c-N,d-N,k,l)*(0.25*t1[b-N,k]*t1[a-N,l]*t2[c-N,d-N,i,j] - t1[c-N,k]*t1[b-N,l]*t2[d-N,a-N,i,j] - 0.5*t2[b-N,c-N,i,j]*t2[a-N,d-N,k,l])
+
+					#P(ij)-terms
+					for c in range(N,L):
+						tdoubles[a-N,b-N,i,j] += QRPS2(c,j,a,b,w)*t1[c-N,i] - QRPS2(c,i,a,b,w)*t1[c-N,j]
+
+					for c in range(N,L):
+						for d in range(N,L):
+							tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(c,d,a,b,w)*t1[c-N,i]*t1[d-N,j] - 0.5*QRPS2(c,d,a,b,w)*t1[c-N,j]*t1[d-N,i]
+
+					for k in range(0,N):
+						for c in range(N,L):
+							tdoubles[a-N,b-N,i,j] += F[k,c]*t1[c-N,i]*t2[a-N,b-N,j,k] - F[k,c]*t1[c-N,j]*t2[a-N,b-N,i,k]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(c,j,k,l,w)*t1[c-N,i]*t2[a-N,b-N,k,l] - QRPS2(c,i,k,l,w)*t1[c-N,k]*t2[a-N,b-N,l,j]
+								tdoubles[a-N,b-N,i,j] -= 0.5*QRPS2(c,i,k,l,w)*t1[c-N,j]*t2[a-N,b-N,k,l] - QRPS2(c,j,k,l,w)*t1[c-N,k]*t2[a-N,b-N,l,i]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								for d in range(N,L):							 						
+									tdoubles[a-N,b-N,i,j] += QRPS2(c,d,k,l,w)*(0.25*t1[c-N,i]*t1[d-N,j]*t2[a-N,b-N,k,l] - t1[c-N,k]*t1[d-N,i]*t2[a-N,b-N,l,j] - 0.5*t2[a-N,b-N,i,k]*t2[c-N,d-N,j,l])
+									tdoubles[a-N,b-N,i,j] -= QRPS2(c,d,k,l,w)*(0.25*t1[c-N,j]*t1[d-N,i]*t2[a-N,b-N,k,l] - t1[c-N,k]*t1[d-N,j]*t2[a-N,b-N,l,i] - 0.5*t2[a-N,b-N,j,k]*t2[c-N,d-N,i,l])
+
+					#P(ij)P(ab)-terms
+
+					for k in range(0,N):
+						for c in range(N,L):
+							tdoubles[a-N,b-N,i,j] += QRPS2(c,j,k,b,w)*t2[a-N,c-N,i,k] - QRPS2(i,c,k,b,w)*t1[a-N,k]*t1[c-N,j]
+							tdoubles[a-N,b-N,i,j] -= QRPS2(c,j,k,a,w)*t2[b-N,c-N,i,k] - QRPS2(i,c,k,a,w)*t1[b-N,k]*t1[c-N,j]
+							tdoubles[a-N,b-N,i,j] -= QRPS2(c,i,k,b,w)*t2[a-N,c-N,j,k] - QRPS2(j,c,k,b,w)*t1[a-N,k]*t1[c-N,i]
+							tdoubles[a-N,b-N,i,j] += QRPS2(c,i,k,a,w)*t2[b-N,c-N,j,k] - QRPS2(j,c,k,a,w)*t1[b-N,k]*t1[c-N,i]
+
+					for k in range(0,N):
+						for c in range(N,L):
+							for d in range(N,L):
+								tdoubles[a-N,b-N,i,j] += QRPS2(d,c,a,k)*t1[d-N,i]*t2[b-N,c-N,j,k] - QRPS2(c,d,k,b,w)*t1[c-N,i]*t1[a-N,k]*t1[d-N,j]
+								tdoubles[a-N,b-N,i,j] -= QRPS2(d,c,b,k)*t1[d-N,i]*t2[a-N,c-N,j,k] - QRPS2(c,d,k,a,w)*t1[c-N,i]*t1[b-N,k]*t1[d-N,j]
+								tdoubles[a-N,b-N,i,j] -= QRPS2(d,c,a,k)*t1[d-N,j]*t2[b-N,c-N,i,k] - QRPS2(c,d,k,b,w)*t1[c-N,j]*t1[a-N,k]*t1[d-N,i]	
+								tdoubles[a-N,b-N,i,j] += QRPS2(d,c,b,k)*t1[d-N,j]*t2[a-N,c-N,i,k] - QRPS2(c,d,k,a,w)*t1[c-N,j]*t1[b-N,k]*t1[d-N,i]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(c,j,k,l,w)*t1[c-N,i]*t1[a-N,k]*t1[b-N,l] + QRPS2(i,c,k,l,w)*t1[a-N,l]*t2[b-N,c-N,j,k]
+								tdoubles[a-N,b-N,i,j] -= 0.5*QRPS2(c,j,k,l,w)*t1[c-N,i]*t1[b-N,k]*t1[a-N,l] + QRPS2(i,c,k,l,w)*t1[b-N,l]*t2[a-N,c-N,j,k]
+								tdoubles[a-N,b-N,i,j] -= 0.5*QRPS2(c,i,k,l,w)*t1[c-N,j]*t1[a-N,k]*t1[b-N,l] + QRPS2(j,c,k,l,w)*t1[a-N,l]*t2[b-N,c-N,i,k]
+								tdoubles[a-N,b-N,i,j] += 0.5*QRPS2(c,i,k,l,w)*t1[c-N,j]*t1[b-N,k]*t1[a-N,l] + QRPS2(j,c,k,l,w)*t1[b-N,l]*t2[a-N,c-N,i,k]
+
+					for k in range(0,N):
+						for l in range(0,N):
+							for c in range(N,L):
+								for d in range(N,L):
+									tdoubles[a-N,b-N,i,j] += QRPS2(c,d,k,l,w)*(t1[c-N]*t1[b-N,l]*t2[a-N,d-N,k,j] + 0.25*t1[c-N,i]*t1[a-N,k]*t1[d-N,j]*t1[b-N,l] + 0.5*t2[a-N,c-N,i,k]*t2[d-N,b-N,l,j])
+									tdoubles[a-N,b-N,i,j] -= QRPS2(c,d,k,l,w)*(t1[c-N]*t1[a-N,l]*t2[b-N,d-N,k,j] + 0.25*t1[c-N,i]*t1[b-N,k]*t1[d-N,j]*t1[a-N,l] + 0.5*t2[b-N,c-N,i,k]*t2[d-N,a-N,l,j])
+									tdoubles[a-N,b-N,i,j] -= QRPS2(c,d,k,l,w)*(t1[c-N]*t1[b-N,l]*t2[a-N,d-N,k,i] + 0.25*t1[c-N,j]*t1[a-N,k]*t1[d-N,i]*t1[b-N,l] + 0.5*t2[a-N,c-N,j,k]*t2[d-N,b-N,l,i])
+									tdoubles[a-N,b-N,i,j] += QRPS2(c,d,k,l,w)*(t1[c-N]*t1[a-N,l]*t2[b-N,d-N,k,i] + 0.25*t1[c-N,j]*t1[b-N,k]*t1[d-N,i]*t1[a-N,l] + 0.5*t2[b-N,c-N,j,k]*t2[d-N,a-N,l,i])
 
 #####################################################################################
 inFile = open('coulomb2.dat','r')
